e5d8d4e20640acdd206240263a59b530275087ee
Vaibhav Mehta <Vaibhav Mehta@LAPTOP-23KI3N94>
2021-10-06 12:44:29 +0530

    by a

--- Testing_GITHub_06_10/src/git/programs/Demo1.java
+++ Testing_GITHub_06_10/src/git/programs/Demo1.java
@@ -2,9 +2,15 @@ package git.programs;
 
 public class Demo1 {
 	
+	public void m1()
+	{
+		System.out.println("Hello This is M1");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("GIT_Hub");
 	}
 
 }
+	
